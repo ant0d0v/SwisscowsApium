2cb2416ce68813cab7c71851d26521ac46ce32fd
ant0d0v <gosu1203@gmail.com>
2023-07-24 09:45:50 +0300

    add capability

--- src/test/java/Base/AndroidBaseUtils.java
+++ src/test/java/Base/AndroidBaseUtils.java
@@ -1,17 +1,18 @@
 package Base;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
 public final class AndroidBaseUtils {
-//  private static final String APPIUM_URL = "http://127.0.0.1:4723/wd/hub";
+    private static final String APPIUM_URL = "http://127.0.0.1:4723/wd/hub";
     private static final String USERNAME = "oauth-gosu1203-b638e";
-    private static final String ACCESS_KEY = "d12a01b0-33d2-4575-80ae-5a0817a307e2";
-    private static final String APPIUM_URL = "https://" + USERNAME + ":" + ACCESS_KEY
-            + "@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
+    //    private static final String ACCESS_KEY = "d12a01b0-33d2-4575-80ae-5a0817a307e2";
+//    private static final String APPIUM_URL = "https://" + USERNAME + ":" + ACCESS_KEY
+//            + "@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
     private static final String ANDROID_PLATFORM_NAME = "Android";
     private static final String TEST_APP = "instagram.apk";
     private static final Logger log = Logger.getLogger(AndroidBaseTest.class.getName());
@@ -22,7 +23,7 @@ public final class AndroidBaseUtils {
 
     static AndroidDriver initDriver(AndroidDriver driver) {
         try {
-            driver = new AndroidDriver(new URL(APPIUM_URL), getSourceLabsCapabilities());
+            driver = new AndroidDriver(new URL(APPIUM_URL), getLocalCapabilities());
             log.info("Appium url: " + APPIUM_URL);
         } catch (Exception ex) {
             throw new RuntimeException("Appium driver could not be initialized." + ex.getMessage());
@@ -51,6 +52,7 @@ public final class AndroidBaseUtils {
         capabilities.setCapability("resetKeyboard", "true");
         return capabilities;
     }
+
     private static DesiredCapabilities getSourceLabsCapabilities() {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -68,4 +70,19 @@ public final class AndroidBaseUtils {
         capabilities.setCapability("appActivity", "com.instagram.mainactivity.MainActivity");
         return capabilities;
     }
+
+//    public static DesiredCapabilities getRealDeviceCapabilities() {
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Galaxy S10");
+//        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+//        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+//        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12");
+//        capabilities.setCapability("appPackage", "com.instagram.android");
+//        capabilities.setCapability("appActivity", "com.instagram.mainactivity.MainActivity");
+//        capabilities.setCapability(MobileCapabilityType.APP, "/Users/antonudovycenko/IdeaProjects/AppiumInstagramDemo/alevel/src/test/resources/" + TEST_APP);
+//        capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+//        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 600);
+//        return capabilities;
+//    }
+
 }
