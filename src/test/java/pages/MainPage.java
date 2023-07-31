a5563da3b1b9b2dbd52fb708538fad51ea192f7c
ant0d0v <gosu1203@gmail.com>
2023-08-01 00:18:35 +0300

    frame

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -5,7 +5,7 @@ import io.appium.java_client.pagefactory.AndroidFindBy;
 import org.openqa.selenium.WebElement;
 import pages.base_abstract.BasePage;
 
-public class MainPage extends BasePage {
+public class MainPage extends  BasePage {
 
 
     @AndroidFindBy(className =  "android.widget.EditText")
@@ -14,7 +14,9 @@ public class MainPage extends BasePage {
     public MainPage(AndroidDriver driver){
         super(driver);
     }
-
+    public MainPage createGeneric() {
+        return new MainPage(driver);
+    }
 
 
     @Override
