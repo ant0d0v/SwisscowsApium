dc1d3ecf5e7776b6bafb49e7bad0a844c08014e9
ant0d0v <gosu1203@gmail.com>
2023-07-31 23:40:44 +0300

    аramework

---
+++ src/test/java/utils/ProjectConstants.java
@@ -0,0 +1,17 @@
+package utils;
+
+public class ProjectConstants {
+
+    public static final String DOMAIN = "https://dev.swisscows.com";
+    public static final String URL_ACCOUNTS = "https://accounts.dev.swisscows.com";
+    public static final String TITLE_VPN_PAGE = "Surf anonymously with VPN - Secure web surfing with Swisscows";
+    public static final String TITLE_MAIN_PAGE = "Your private and anonymous search engine Swisscows";
+    public static final String DOMAIN_WITHOUT_DEV = "https://swisscows.com";
+    public static final String PASSWORD = "2075Deltuha";
+    public static final String SWISSCOWS_EMAIL_USER = "a.qa@swisscows.email";
+    public static final String FONT_SIZE_40_PX = "40px";
+
+    public static final int WIDTH_HAMBURGER_MENU = 224;
+    public static final int WIDTH_TABLET = 768;
+    public static final int HEIGHT_HAMBURGER_MENU = 900;
+}
---
+++ src/test/java/utils/TestUtils.java
@@ -0,0 +1,94 @@
+package utils;
+
+import Base.AndroidBaseTest;
+import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+
+public class TestUtils {
+    private final static By H2_HEADER = By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]");
+
+    public static void loadBaseUrlPage(AndroidDriver driver, WebDriverWait wait) {
+        driver.launchApp();
+    }
+
+    public static void reLoadBaseUrlPage(AndroidDriver driver, WebDriverWait wait) {
+        int count = 0;
+        while (count <= 3 && !(isH2HeaderExists(driver))) {
+            loadBaseUrlPage(driver, wait);
+            count++;
+        }
+
+        if (count == 3 && !isH2HeaderExists(driver)) {
+            Reporter.log("!!!!! Error !!!!! BaseURL page was NOT loaded. Re-Run jobs\n", true);
+        }
+    }
+
+    public static boolean isH2HeaderExists(AndroidDriver driver) {
+        boolean isExists = true;
+        try {
+            driver.findElement(H2_HEADER);
+        } catch (NoSuchElementException e) {
+            isExists = false;
+        }
+        return isExists;
+    }
+
+    public static String getRandomName(int length) {
+
+        return RandomStringUtils
+                .random(length,
+                        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+    }
+    public static String getRandomNameForBrazilBots(int length) {
+
+        return RandomStringUtils
+                .random(length,
+                        "abcdefghijklmnopqrst");
+    }
+
+    public static String getRandomName() {
+
+        return getRandomName(7);
+    }
+    public static String getRandomNameForBrazilBots() {
+
+        return getRandomNameForBrazilBots(7);
+    }
+
+    public static int convertStringToInt(String text) {
+
+        return Integer.parseInt(text);
+    }
+
+    public static String getSubstring(String text, String separator) {
+        int index = text.indexOf(separator);
+
+        return text.substring(0, index);
+    }
+
+    public static void addCookie(WebDriver driver, String cookieValue){
+        Date expDate = new Date();
+        expDate.setTime(expDate.getTime() + (10000 * 10000));
+        Cookie cookie = new Cookie(".AspNetCore.Identity.Application", cookieValue, "accounts.dev.swisscows.com", "/", expDate);
+        driver.manage().addCookie(cookie);
+    }
+
+    public static List<String> getSortedList(List<String> elements) {
+
+        return elements.stream().sorted().collect(Collectors.toList());
+    }
+}
